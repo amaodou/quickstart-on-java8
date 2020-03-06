27eb7e88d8ba5e441ba5a31b4c85321391d3ba71
lizhiyong36 <lizhiyong5@jd.com>
2020-03-06 16:53:15 +0800

    add leetcode solution

---
+++ src/main/java/xyz/mdou/quickstart/leetcode/InsertSortList.java
@@ -0,0 +1,73 @@
+package xyz.mdou.quickstart.leetcode;
+
+/**
+ * 对链表进行插入排序。
+ *
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ *
+ * 插入排序算法：
+ *
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ *  
+ * 示例 1：
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ *
+ * 示例 2：
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ * 链接：https://leetcode-cn.com/problems/insertion-sort-list
+ */
+public class InsertSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode sorted = head;
+        while (sorted.next != null) {
+            ListNode nextInsert = sorted.next;
+            sorted.next = nextInsert.next;
+            nextInsert.next = null;
+
+            ListNode current = head;
+            ListNode pre = null;
+            while (current != sorted.next) {
+                if (current.val < nextInsert.val) {
+                    if (current == sorted) {
+                        nextInsert.next = sorted.next;
+                        sorted.next = nextInsert;
+                        sorted = nextInsert;
+                        break;
+                    }
+                    pre = current;
+                    current = current.next;
+                } else {
+                    nextInsert.next = current;
+                    if (pre == null) {
+                        head = nextInsert;
+                    } else {
+                        pre.next = nextInsert;
+                    }
+                    break;
+                }               
+            }
+        }
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/main/java/xyz/mdou/quickstart/leetcode/ReplaceSpace.java
@@ -0,0 +1,63 @@
+package xyz.mdou.quickstart.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *
+ * 限制：
+ * 0 <= s 的长度 <= 10000
+ *
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ */
+public class ReplaceSpace {
+
+    public String replaceSpace(String s) {
+        if (s == null) {
+            return s;
+        }
+        long spaceCount = s.chars().filter(c -> c == ' ').count();
+        if (spaceCount == 0) {
+            return s;
+        }
+        char[] newChars = new char[(int) (s.length() + spaceCount * 2)];
+        int newIndex = newChars.length - 1;
+        int originIndex = s.length() - 1;
+        while (newIndex >= 0) {
+            char o = s.charAt(originIndex);
+            if (s.charAt(originIndex) == ' ') {
+                newChars[newIndex--] = '0';
+                newChars[newIndex--] = '2';
+                newChars[newIndex] = '%';
+            } else {
+                newChars[newIndex] = s.charAt(originIndex);
+            }
+            originIndex -= 1;
+            newIndex -=1;
+        }
+        return new String(newChars);
+    }
+
+    @Test
+    public void testReplaceNull() {
+        Assert.assertNull(replaceSpace(null));
+    }
+
+    @Test
+    public void testReplaceEmpty() {
+        Assert.assertEquals("", replaceSpace(""));
+    }
+
+    @Test
+    public void testReplaceNormal() {
+        String s = "We are happy.";
+        String expect = "We%20are%20happy.";
+        Assert.assertEquals(expect, replaceSpace(s));
+    }
+}
