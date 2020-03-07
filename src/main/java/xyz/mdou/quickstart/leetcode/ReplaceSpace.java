2752e4c60fdb0fbaefd2849d03f41fda14ec1923
lizhiyong36 <lizhiyong5@jd.com>
2020-03-07 16:40:22 +0800

    补充题目描述

--- src/main/java/xyz/mdou/quickstart/leetcode/ReplaceSpace.java
+++ src/main/java/xyz/mdou/quickstart/leetcode/ReplaceSpace.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ * 面试题05. 替换空格
+ *
  * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
  *
  * 示例 1：
--- src/main/java/xyz/mdou/quickstart/leetcode/ReversePrintNode.java
+++ src/main/java/xyz/mdou/quickstart/leetcode/ReversePrintNode.java
@@ -6,7 +6,23 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Stack;
 
-
+/**
+ * 面试题06. 从尾到头打印链表
+ *
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 10000
+ *
+ * 链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+ */
 public class ReversePrintNode {
 
     @ToString
